7edf8937090fd99d48b8da59ef040508bdbbebc2
makksi <makksim@hotmail.com>
2013-03-05 18:13:02 +0100

    cleaning

--- Vodafone/src/com/makksi/vodafone/SettingsActivity.java
+++ Vodafone/src/com/makksi/vodafone/SettingsActivity.java
@@ -1,6 +1,7 @@
+// cleaning
 package com.makksi.vodafone;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
@@ -9,13 +10,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
 public class SettingsActivity extends Activity {
 	SharedPreferences preferences;	
-//	public static String userValue ="";
-//	public static String passValue ="";
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
